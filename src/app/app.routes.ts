27590ef15de338696943370188eb721e6e245751
Kseniia Chepur <kseniia.chepur@gmail.com>
2025-01-26 15:17:24 +0200

    add responsive toolbar and sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'new-contact', component: NewContactComponent },
+];
